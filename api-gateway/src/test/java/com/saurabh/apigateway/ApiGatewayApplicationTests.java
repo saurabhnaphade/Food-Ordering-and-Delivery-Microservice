2c8c44eacd6ff7d74b1f12e59dc948c0a61a6a68
Saurabh Naphade <naphadesaurabh@gmail.com>
2021-08-12 21:52:50 +0530

    Api-Gateway  , Naming-Server And Customer Service Projects Created

---
+++ api-gateway/src/test/java/com/saurabh/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saurabh.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
